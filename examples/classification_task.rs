dfa220285bac76f883e3a62012a0284c2b5eb20a
Martin Billinger <flkazemakase@gmail.com>
2018-06-29 19:56:48 +0200

    feat: examples

---
+++ examples/classification_task.rs
@@ -0,0 +1,29 @@
+extern crate openml;
+
+use openml::prelude::*;
+use openml::{PredictiveAccuracy, SupervisedClassification};
+use openml::baseline::NaiveBayesClassifier;
+
+fn main() {
+    // Load "Supervised Classification on iris" task (https://www.openml.org/t/59)
+    let task = SupervisedClassification::from_openml(59).unwrap();
+
+    println!("Task: {}", task.name());
+
+    // run the task
+    let result: PredictiveAccuracy<_> = task.run(|train, test| {
+        // train classifier
+        let nbc: NaiveBayesClassifier<u8> = train
+            .map(|(x, y)| (x, y))
+            .collect();
+
+        // test classifier
+        let y_out: Vec<_> = test
+            .map(|x| nbc.predict(x))
+            .collect();
+
+        Box::new(y_out.into_iter())
+    });
+
+    println!("Classification Accuracy: {}", result.result());
+}
---
+++ examples/regression_task.rs
@@ -0,0 +1,29 @@
+extern crate openml;
+
+use openml::prelude::*;
+use openml::{RootMeanSquaredError, SupervisedRegression};
+use openml::baseline::NaiveLinearRegression;
+
+fn main() {
+    // Load "Supervised Regression on liver-disorders" task (https://www.openml.org/t/52948)
+    let task = SupervisedRegression::from_openml(52948).unwrap();
+
+    println!("Task: {}", task.name());
+
+    // run the task
+    let result: RootMeanSquaredError<_> = task.run(|train, test| {
+        // train model
+        let model: NaiveLinearRegression = train
+            .map(|(x, y)| (x, y))
+            .collect();
+
+        // test model
+        let y_out: Vec<_> = test
+            .map(|x| model.predict(x))
+            .collect();
+
+        Box::new(y_out.into_iter())
+    });
+
+    println!("Root Mean Squared Error: {}", result.result());
+}
