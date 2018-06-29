c94ae7539baa0109f388400318af867e9a45aa6a
Martin Billinger <flkazemakase@gmail.com>
2018-06-29 21:39:03 +0200

    feat: add Adjusted Rand Index measure

--- src/measure_accumulator.rs
+++ src/measure_accumulator.rs
@@ -1,7 +1,11 @@
 //! Measure accumulators are summaries of model performance, such as classification accuracy or
 //! regression error.
 
+use std::cmp::Eq;
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::marker::PhantomData;
+
 use num_traits::AsPrimitive;
 
 /// Trait implemented by performance measures
@@ -85,4 +89,75 @@ where
     fn result(&self) -> f64 {
         (self.sum_of_squares / self.n as f64).sqrt()
     }
+}
+
+
+/// Adjusted Rand Index
+#[derive(Debug)]
+pub struct AdjustedRandIndex<T>
+where T: Eq + Hash,
+{
+    contingency_table: HashMap<(T, T), usize>
+}
+
+
+
+impl<T> MeasureAccumulator<T> for AdjustedRandIndex<T>
+    where T: Eq + Hash + Clone,
+{
+    fn new() -> Self {
+        AdjustedRandIndex {
+            contingency_table: HashMap::new()
+        }
+    }
+
+    fn update_one(&mut self, known: &T, pred: &T) {
+        let n = self.contingency_table
+            .entry((known.clone(), pred.clone()))
+            .or_insert(0);
+        *n += 1;
+    }
+
+    fn result(&self) -> f64 {
+        let mut a = HashMap::new();
+        let mut b = HashMap::new();
+
+        let mut ri = 0usize;
+        let mut n_tot = 0usize;
+
+        for ((ak, bk), &n) in self.contingency_table.iter() {
+            n_tot += n;
+            ri += combinations(n);
+
+            *a.entry(ak).or_insert(0usize) += n;
+            *b.entry(bk).or_insert(0usize) += n;
+        }
+
+        let a_sum: usize = a.iter().map(|(_, &n)| combinations(n)).sum();
+        let b_sum: usize = b.iter().map(|(_, &n)| combinations(n)).sum();
+
+        let expected_ri = (a_sum as f64) * (b_sum as f64) / combinations(n_tot) as f64;
+        let max_ri = (a_sum + b_sum) as f64 / 2.0;
+
+        (ri as f64 - expected_ri) / (max_ri - expected_ri)
+    }
+}
+
+fn combinations(n: usize) -> usize {
+    if n % 2 == 0 {
+        (n - 1) * (n / 2)
+    } else {
+        n * ((n - 1) / 2)
+    }
+}
+
+#[test]
+fn ari() {
+    let labels_true = [0, 0, 0, 1, 1, 1];
+    let labels_pred = [0, 0, 1, 1, 2, 2];
+
+    let mut ari = AdjustedRandIndex::new();
+    ari.update(labels_true.iter(), labels_pred.iter());
+
+    assert_eq!(ari.result(), 0.24242424242424246);
 }
