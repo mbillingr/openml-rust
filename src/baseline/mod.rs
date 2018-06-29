501216f04c8aab1e2e88e22561f7aaf53445dc63
Martin Billinger <flkazemakase@gmail.com>
2018-06-29 19:56:30 +0200

    feat: add Naive Linear Regression model
    Closes #1

--- src/baseline/mod.rs
+++ src/baseline/mod.rs
@@ -1,5 +1,7 @@
 //! Implementation of simple baseline models, used for testing and demonstration.
 
 mod naive_bayes_classifier;
+mod naive_linear_regression;
 
 pub use self::naive_bayes_classifier::NaiveBayesClassifier;
+pub use self::naive_linear_regression::NaiveLinearRegression;
---
+++ src/baseline/naive_linear_regression.rs
@@ -0,0 +1,103 @@
+//! Implementation of a Naive Linear Regression model
+
+use std::f64;
+use std::iter::FromIterator;
+
+/// A Naive Linear Regression model
+///
+/// This is univariate regression on a single feature. During training the best feature is selected.
+/// The model is trained by consuming an iterator over the training data:
+/// ```
+/// # use openml::baseline::NaiveLinearRegression;
+/// # let data: Vec<(&[f64], &f64)> = vec![];
+/// let model: NaiveLinearRegression = data
+///     .into_iter()
+///     .collect();
+/// ```
+#[derive(Debug)]
+pub struct NaiveLinearRegression
+{
+    slope: f64,
+    intercept: f64,
+    feature: usize,
+}
+
+impl<'a, J> FromIterator<(J, &'a f64)> for NaiveLinearRegression
+    where
+        J: IntoIterator<Item=&'a f64>,
+{
+    fn from_iter<I: IntoIterator<Item=(J, &'a f64)>>(iter: I) -> Self {
+        let mut feature_columns = Vec::new();
+        let mut target_column = Vec::new();
+
+        for (x, &y) in iter {
+            target_column.push(y);
+            for (i, &xi) in x.into_iter().enumerate() {
+                if i >= feature_columns.len() {
+                    feature_columns.push(Vec::new());
+                }
+
+                feature_columns[i].push(xi);
+            }
+        }
+
+        let mut y_mean = 0.0;
+        for y in &target_column {
+            y_mean += *y;
+        }
+        y_mean /= target_column.len() as f64;
+
+        let mut best_err = f64::INFINITY;
+        let mut best_slope = f64::NAN;
+        let mut best_intercept = f64::NAN;
+        let mut best_feature = 0;
+
+        for (i, feature) in feature_columns.iter().enumerate() {
+            let mut x_mean = 0.0;
+            for x in feature {
+                x_mean += *x;
+            }
+            x_mean /= feature.len() as f64;
+
+            let mut x_var = 0.0;
+            let mut covar = 0.0;
+            for (x, y) in feature.iter().zip(target_column.iter()) {
+                let x = *x - x_mean;
+                let y = *y - y_mean;
+
+                x_var += x * x;
+                covar += x * y;
+            }
+
+            let slope = covar / x_var;
+            let intercept = y_mean - slope * x_mean;
+
+            let err: f64 = feature.iter()
+                .zip(target_column.iter())
+                .map(|(&x, &y)| intercept + slope * x - y)
+                .map(|r| r * r)
+                .sum();
+
+            if err < best_err {
+                best_err = err;
+                best_slope = slope;
+                best_intercept = intercept;
+                best_feature = i;
+            }
+        }
+
+        NaiveLinearRegression {
+            slope: best_slope,
+            intercept: best_intercept,
+            feature: best_feature,
+        }
+    }
+}
+
+impl NaiveLinearRegression
+{
+    /// predict target value for a single feature vector
+    pub fn predict(&self, x: &[f64]) -> f64 {
+        self.intercept + x[self.feature] * self.slope
+    }
+}
